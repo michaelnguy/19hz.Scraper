9e8ed5a0860c6b8f7a4d0a13e949ff20d3522620
vorger <michaelnguyencs@gmail.com>
2018-09-03 11:17:17 -0700

    added headless mode and JSON sorting w/ cmdline args

--- 19hzScraper.py
+++ 19hzScraper.py
@@ -7,35 +7,65 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
-import json
+from selenium.webdriver.chrome.options import Options
+import os
+import json, operator
 import HTMLParser
 from progress.bar import IncrementalBar
+import sys
 
-# Run Chrome Webdriver, logs into 19hz.info, and ensures we're on the right page
-driver = webdriver.Chrome()
-driver.get("https://19hz.info/eventlisting_BayArea.php")
-assert "Event Listing" in driver.title
-
-# Waits for the page to load before we begin scraping data
-try:
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//tbody[1]')))
-except TimeoutException:
-    pass  # Handle the exception here
-
-# Locates the table of data, checks for how many rows and columns
-table = driver.find_element_by_xpath('//tbody[1]').get_attribute('innerHTML')
-rows = len(driver.find_elements_by_xpath("//table[1]/tbody/tr"))
-columns = len(driver.find_elements_by_xpath("//table[1]/thead/tr/th"))
-
-# Initialize a progress bar to visualize progress on the webscrape
-progress_col = columns - 1;
-bar = IncrementalBar('Gathering', max = rows * progress_col)
+# To avoid any unicode encoding/decoding errors
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 # This HTMLParser is required to unescape HTML syntax into human language
 html = HTMLParser.HTMLParser()
 
+# Available filter keys
+filter = ['age', 'name', 'price', 'datetime', 'location']
+
+# Global variables
+driver = None
+mode = False
+sort = None
+
+def browser(headless, filter):
+
+	global driver
+
+	# Decides whether driver is headless or not
+	if headless:
+		chrome_options = Options()
+		chrome_options.add_argument("--headless")
+		driver = webdriver.Chrome(chrome_options=chrome_options)
+	else:
+		driver = webdriver.Chrome()
+
+	# Logs into 19hz.info, and ensures we're on the right page
+	driver.get("https://19hz.info/eventlisting_BayArea.php")
+	assert "Event Listing" in driver.title
+
+	# Waits for the page to load before we begin scraping data
+	try:
+	    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//tbody[1]')))
+	except TimeoutException:
+	    pass  # Handle the exception here
+
+	# Begin scraping data
+	gather(filter)
+
 # Main function to scrape data
-def gather():
+def gather(filter):
+
+	# Locates the table of data, checks for how many rows and columns
+	table = driver.find_element_by_xpath('//tbody[1]').get_attribute('innerHTML')
+	rows = len(driver.find_elements_by_xpath("//table[1]/tbody/tr"))
+	columns = len(driver.find_elements_by_xpath("//table[1]/thead/tr/th"))
+
+	# Initialize a progress bar to visualize progress on the webscrape
+	progress_col = columns - 1;
+	bar = IncrementalBar('> Generating JSON file ...', max = rows * progress_col, suffix='%(percent)d%%')
+
 	# Data Structure for the scrapped data
 	data = {}
 	data['events'] = []
@@ -55,7 +85,7 @@ def gather():
 			elif y == 2: # Event Name, Location, and Link Column
 				# Getting location data
 				full = datum.get_attribute('innerHTML')
-				full = str(full).split('@')
+				full = full.encode('utf-8').split('@')
 				location = full[1].strip()
 				# Getting event name data
 				link = datum.find_element_by_tag_name('a')
@@ -111,13 +141,42 @@ def gather():
 					temp['organizer'] = organizer
 			bar.next() # Shows data scraping progress through the table
 		data['events'].append(temp)
+
+	# If you like to sort the JSON by a specific key
+	if filter:
+		data['events'].sort(key=operator.itemgetter(filter))
+		print('\n> Data sorted by: ' + filter)
+
 	# After we're done iterating the table, we'll dump the data into a json file for external application usage
 	with open('data.json', 'w') as outfile:
 		json.dump(data, outfile, indent = 4, sort_keys=True)
+		if filter:
+			print('> Done.')
+		else:
+			print('\n> Done.')
+		print('> JSON file generated to current directory.')
 	bar.finish() # Loading Bar finishes
 
-# Run the main function
-gather()
+# Checking for command line arguments
+for arg in sys.argv:
+	if arg == '-hl':
+		mode = True
+	elif not sort and arg in filter:
+		sort = arg
+
+# If user has specified headless mode or not
+if mode:
+	print('> Running in headless mode (Press CRTL+C to cancel)')
+else:
+	print('> Running in head mode (Press CRTL+C to cancel)')
+
+# If user has specified sorting for the JSON
+if sort:
+	print('> Detected JSON sort key: ' + sort)
+else:
+	print('> No JSON sort key found. (Available sort keys: age name price datetime location)')
+
+browser(mode, sort)
 
 # Close the browser when we're done
 driver.close()
